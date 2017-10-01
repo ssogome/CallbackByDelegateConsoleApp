5f2adc52a64997ba739518c177bb7aa631142e54
Pap Sogome <ssogome@gmail.com>
2017-10-01 11:37:45 -0600

    Add get DNS information of host computer

--- CallbackByDelegateConsoleApp/Program.cs
+++ CallbackByDelegateConsoleApp/Program.cs
@@ -1,15 +1,83 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CallbackByDelegateConsoleApp
 {
     class Program
     {
+        static int requestCounter;
+        static ArrayList hostData = new ArrayList();
+        static StringCollection hostNames = new StringCollection();
+        static void UpdateUserInterface()
+        {
+            Console.WriteLine("{0} requests remaining. ", requestCounter);
+        }
+
         static void Main(string[] args)
         {
+            //Create a delegate that will process the results of the asynchronous request
+            AsyncCallback callBack = new AsyncCallback(ProcessDnsInformation);
+            string host;
+            do
+            {
+                Console.Write("Enter the name of the computer or <enter> to finish.");
+                host = Console.ReadLine();
+                if (host.Length > 0)
+                {
+                    //Increment the request count in a thread-safe manner
+                    Interlocked.Increment(ref requestCounter);
+                    //Start the asynchromous request for DNS information
+                    Dns.BeginGetHostEntry(host, callBack, host);
+                }
+            } while (host.Length > 0);
+
+            // The user has entered all of the host names for lookup. Now wait until the threads complete.
+            while(requestCounter > 0)
+            {
+                UpdateUserInterface();
+            }
+            //Display the result
+            for(int i=0; i< hostNames.Count; i++)
+            {
+                object data = hostData[i];
+                string message = data as string;
+                if(message != null)
+                {
+                    Console.WriteLine("Request for {0} returned message : {1}", hostNames[i], message);
+                    continue;
+                }
+                //Get the results
+                IPHostEntry h = (IPHostEntry)data;
+                string[] aliases = h.Aliases;
+                IPAddress[] addresses = h.AddressList;
+                if(aliases.Length > 0)
+                {
+                    Console.WriteLine("Aliases for {0}", hostNames[i]);
+                    for(int j=0; j< aliases.Length; j++)
+                    {
+                        Console.WriteLine("{0}", aliases[j]);
+                    }
+                }
+                if(addresses.Length > 0)
+                {
+                    Console.WriteLine("Addresses for {0}", hostNames[i]);
+                    for(int k=0; k< addresses.Length; k++)
+                    {
+                        Console.WriteLine("{0}", addresses[k].ToString());
+                    }
+                }
+            }
+
+            
+
             CallbackTest callbackTest = new CallbackTest();
             callbackTest.Test();
 
@@ -17,7 +85,30 @@ namespace CallbackByDelegateConsoleApp
 
             MeetingExecution mtingExec = new MeetingExecution();
             mtingExec.PerformingMeeting();
+
+           
             Console.ReadKey();
+        }
+
+        static void ProcessDnsInformation(IAsyncResult result)
+        {
+            string hostName = (string)result.AsyncState;
+            hostNames.Add(hostName);
+            try
+            {
+                //Get results
+                IPHostEntry host = Dns.EndGetHostEntry(result);
+                hostData.Add(host);
+            }
+            catch(SocketException ex)
+            {
+                hostData.Add(ex.Message);
+            }
+            finally
+            {
+                //Decrement the request counter in a thread-safe manner
+                Interlocked.Decrement(ref requestCounter);
+            }
         }
     }
 }
---
+++ CallbackByDelegateConsoleApp/TryCatchFinally.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbackByDelegateConsoleApp
+{
+    public class TryCatchFinally
+    {
+        public void ReadFile(int index)
+        {
+            string path = @"C:\Users\User\Desktop\Infosys_OnBoarding.pdf";
+            StreamReader file = new StreamReader(path);
+            char[] buffer = new char[10000];
+            try
+            {
+                file.ReadBlock(buffer, index, buffer.Length);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error reading from {0}. Message  = {1}", path, e.Message);
+            }
+            finally
+            {
+                if(file != null)
+                {
+                    file.Close();
+                }
+            }
+        }
+    }
+}
